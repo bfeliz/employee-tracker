a46eb02c5a6e9d07977e684491e3ab1c6bcc7a95
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-03-06 16:32:35 -0700

    Set up database, tables, and autopopulate with employees

---
+++ seed.sql
@@ -0,0 +1,55 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES ("hr");
+
+INSERT INTO department (name)
+VALUES ("business_office");
+
+INSERT INTO department (name)
+VALUES ("nursing");
+
+INSERT INTO department (name)
+VALUES ("therapy");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("hr director", 50000.00, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("bom", 60000.00, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("abom", 30000.00, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("don", 80000.00, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("adon", 60000.00, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("dor", 80000.00, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("therapist", 70000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("janet", "grinch", 1, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("betty", "fuentes", 2, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("kim", "otter", 3, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("linda", "apple", 4, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("vikki", "tanner", 5, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("kelly", "daniels", 4, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("scott", "rogers", 4, 6);
